880b93643b8d31862f9b754aec6f41dc5c234a4f
Max <max@web-development.cc>
2013-03-29 22:11:50 +0100

    initial

---
+++ js/mcf.js
@@ -0,0 +1,44 @@
+var ctx = new webkitAudioContext()
+    , osc = []
+    , octave = 5
+
+$(function () {
+
+    $('.options select').on('change', function () {
+
+        $('.pads .pad').removeClass('highlight');
+
+        var key = $('.options select#key').val();
+        var intervals = $('.options select#type option:selected').data('intervals');
+
+        if (null === key || null == intervals) {
+            return;
+        }
+
+        key += octave;
+        var root = Note.fromLatin(key);
+
+        var chord = root.add(intervals);
+
+        console.log(chord);
+        $(chord).each(function (key) {
+            $('.pads .pad[data-key="' + this.latin() + '"], .pads .pad[data-key-2="' + this.latin() + '"]')
+                .addClass('highlight')
+                .attr('data-interval', key);
+
+            console.log(this.latin());
+        });
+
+        //create oscillators and play sound
+//        $(chord).each(function (key) {
+//            var time = ctx.currentTime;
+//            osc[key] = ctx.createOscillator();
+//            osc[key].type = 0;
+//            osc[key].connect(ctx.destination);
+//            osc[key].frequency.value = this.frequency().toFixed(2);
+//            osc[key].start(time);
+//            osc[key].stop(time + 1);
+//        });
+
+    });
+});
