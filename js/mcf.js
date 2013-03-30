513da6bbb87790efb81da4545b7fba0eff134da5
Max <max@web-development.cc>
2013-03-30 10:36:00 +0100

    collection save, load

--- js/mcf.js
+++ js/mcf.js
@@ -1,25 +1,22 @@
-var ctx = new webkitAudioContext()
-    , osc = []
-    , octave = 3;
-
 $(function () {
 
-    $('.options select').on('change', function () {
+    $('.options input').button();
+
+
+    $('.options label').on('click', function () {
 
         $('.current .pads .pad')
             .removeClass('highlight')
             .find('.interval').html('');
 
-        var key = $('.options select#key').val();
-        var intervals = $('.options select#type option:selected').data('intervals');
+        var key = $('fieldset#key label.ui-state-active').data('key');
+        var intervals = $('fieldset#type label.ui-state-active').data('intervals');
 
-        if (null === key || null == intervals) {
+        if (null === key || undefined === key || null === intervals || undefined === intervals) {
             return;
         }
 
-        key += octave;
         var root = Note.fromLatin(key);
-
         var chord = root.add(intervals);
 
         $(chord).each(function (key) {
@@ -31,8 +28,6 @@ $(function () {
                 .addClass('highlight')
                 .attr('data-interval', interval_class)
                 .find('.interval').html(this.latin() + ' - ' + interval);
-
-            console.log(this.latin());
         });
 
     });
@@ -40,8 +35,8 @@ $(function () {
     $('a#add').on('click', function (e) {
         e.preventDefault();
 
-        var name = $('.options select#key').val();
-        name += ' ' + $('.options select#type option:selected').html();
+        var name = $('fieldset#key label.ui-state-active .ui-button-text').html()
+        name += ' ' + $('fieldset#type label.ui-state-active .ui-button-text').html();
 
         var nameLabel = $('<div/>')
             .addClass('name')
@@ -58,12 +53,67 @@ $(function () {
             .prepend(nameLabel)
             .appendTo('.collection')
             .find('.interval').remove();
+
+        refreshHash();
+
     });
 
+    //clear collection
+    $('a#clear').on('click', function (e) {
+        e.preventDefault();
+        $('.collection .pads .remove').each(function () {
+            $(this).click();
+        })
+    });
+
+    //remove chord from collection
     $('.collection').delegate('.pads .remove', 'click', function (e) {
         e.preventDefault();
         ($(this).parent().fadeOut(function () {
             $(this).remove();
+            refreshHash();
         }));
-    })
+    });
+
+    //activate first chord on load
+    $('.options fieldset#key label').first().click();
+    $('.options fieldset#type label').first().click();
+
+    //load collection from URL
+    try {
+        var toLoad = JSON.parse(decodeURIComponent(location.hash.substring('1')));
+
+        $(toLoad).each(function () {
+            var chord = this.split('_');
+
+            $('.options #key label').filter(function () {
+                if ($(this).text() == chord[0]) {
+                    this.click();
+                }
+            });
+
+            $('.options #type label').filter(function () {
+                if ($(this).text() == chord[1]) {
+                    this.click();
+                }
+            });
+
+            $('a#add').click();
+
+        });
+
+    }
+    catch (e) {
+    }
+
+    //store collection in URL hash
+    function refreshHash() {
+        var collection = [];
+        $('.collection .pads').each(function () {
+            var name = $(this).find('.name').html().replace(' ', '_');
+            collection.push(name);
+        });
+
+        location.hash = JSON.stringify(collection);
+    }
 });
