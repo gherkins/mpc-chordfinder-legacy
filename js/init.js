9b839e35c3556b12df059b0b142f5e005bc33832
Max Gherkins <maxgirkens@gmail.com>
2013-12-30 12:02:21 +0100

    sass, minify js, foundation

---
+++ js/init.js
@@ -0,0 +1,67 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-38888541-2']);
+_gaq.push(['_trackPageview']);
+
+(function () {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+})();
+
+$(window).load(function () {
+
+    var fadeSpeed = 1500;
+
+    $('#joyRideTipContent').joyride({
+        tipAnimation: 'fade',
+        cookieMonster: false,
+        autoStart: true,
+        nextButton: false,
+        preStepCallback: function (index, tip) {
+            switch (index) {
+                case 2:
+                    $('#key button').on('click.tour', function () {
+                        tip.find('.joyride-next-tip').click();
+                    });
+                    break;
+                case 3:
+                    $('#type button').on('click.tour', function () {
+                        tip.find('.joyride-next-tip').click();
+                    });
+                    break;
+                case 5:
+                    $('#add').on('click.tour', function () {
+                        tip.find('.joyride-next-tip').click();
+                    });
+                    break;
+            }
+        },
+        postStepCallback: function (index) {
+            switch (index) {
+                case 2:
+                    $('#key button').off('click.tour');
+                    break;
+                case 3:
+                    $('#type button').off('click.tour');
+                    break;
+                case 5:
+                    $('#add').off('click.tour');
+                    if (0 == $('.collection .pads').length) {
+                        $('button#add').click();
+                    }
+                    break;
+            }
+        },
+        modal: false,
+        expose: true
+    });
+
+    $('a.tour-close').on('click', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $(this).parent().find('.joyride-close-tip').click();
+    });
+});
---
+++ sass/config.rb
@@ -0,0 +1,9 @@
+require "zurb-foundation"
+
+css_dir = "../css"
+sass_dir = "."
+
+output_style = :compressed
+#:expanded or :nested or :compact or :compressed
+
+line_comments = false
